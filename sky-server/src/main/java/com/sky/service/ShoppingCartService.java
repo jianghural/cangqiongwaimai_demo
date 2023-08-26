805816417e76858f42371624c4a729ef5a2fc638
awl <2959687203@qq.com>
2023-08-26 20:28:31 +0800

    day07 添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+
+/**
+ * @author awl
+ * @date 2023/8/26 19:35
+ */
+public interface ShoppingCartService {
+
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
